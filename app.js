b78ec40e838e122fb15c0ab9b4921fc556802b5a
kimi.casamina <kimi.casamina@gmail.com>
2024-07-31 11:16:27 +0800

    refactor database.js

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const mysql = require("mysql2");
 const path = require("path");
 const dotenv = require("dotenv");
-const session = require("express-session");
-const { getUsers } = require("./database");
-const { userInfo } = require("os");
-getUsers;
+const { getUser, verifyUser } = require("./database.js");
+getUser;
+verifyUser;
 
 const app = express();
 dotenv.config();
@@ -15,25 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60 * 1000 * 30,
-    },
-  })
-);
-
 // ------ routes -------
 app.get("/", function (req, res) {
-  console.log("REQ: ", req);
-  const user = req.session.user;
-  if (user) {
-    console.log("IS AUTHENTICATED? ", user);
-    return res.redirect("/dashboard");
-  }
   res.sendFile(path.join(staticFolder, "index.html"));
 });
 
@@ -60,41 +42,30 @@ app.get("/login", function (req, res) {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  if (username === "Saucebot") {
-    console.log(username + " " + password);
-    req.session.user = username;
-    req.session.opp = 1;
-    console.log("SESSION: ", req.session);
-    // res.redirect("/dashboard");
-    res.json({ status: "success", message: "Login Succeed", username });
-    // const users = await getUsers();
-    // console.log("USERS: ", users);
-    // res.json({ message: "Logged in Successfully" });
-    // res.end();
-  } else {
-    res.json({ status: "error", message: "Login Failed" });
-  }
+  console.log(username + " " + password);
+  if (username && password) {
+    try {
+      // check if username exists
+      const user = await getUser(username);
+      if (user.length === 0) {
+        return res.json({ status: "error", message: "Login Failed" });
+      }
 
-  // create db and tables
-  // const db = await createDb();
-  // console.log("DB: ", db);
-});
+      // check if password is valid
+      const isValid = await verifyUser(username, password, user);
+      if (!isValid) {
+        return res.json({ status: "error", message: "Login Failed" });
+      }
 
-app.get("/logout", function (req, res) {
-  if (req.session.user) {
-    req.session.destroy(function () {
-      res.redirect("/");
-    });
+      return res.json({ status: "success", message: "Login Success" });
+    } catch (error) {
+      return res.json({ status: "error", message: "Login Failed" });
+    }
   }
 });
 
 app.get("/dashboard", function (req, res) {
   res.sendFile(path.join(staticFolder, "dashboard.html"));
-  // if (req.session.user) {
-  //   // res.json({ message: "You are logged in" });
-  // } else {
-  //   res.send("You are not authenticated, please logged in.");
-  // }
 });
 
 app.get("*", function (req, res) {
--- database.js
+++ database.js
@@ -12,14 +12,29 @@ const pool = mysql
   })
   .promise();
 
-async function getUsers() {
-  const [rows] = await pool.query("SELECT * FROM users;");
-  console.log("LIST OF USERS", rows);
+async function getUser(username) {
+  const [rows] = await pool.query("SELECT * FROM users WHERE name = ?;", [
+    username,
+  ]);
+
+  return rows;
 }
 
-async function getUser(id) {
-  const [rows] = await pool.query("SELECT * FROM users;");
-  console.log("LIST OF USERS", rows);
+async function verifyUser(username, password, user) {
+  const hashedPassword = user[0].password;
+  const [results] = await pool.query(
+    "SELECT *, MD5(CONCAT(?, ?)) AS confirm FROM users WHERE password = ? ;",
+    [username, password, hashedPassword]
+  );
+
+  if (results[0].password === results[0].confirm) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  console.log("RESULTS: ", results);
+  return isValid;
 }
 
-module.exports = { getUser, getUsers };
+module.exports = { getUser, verifyUser };
